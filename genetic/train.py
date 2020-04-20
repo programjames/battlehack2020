b8a85bf8fd9da08ef3d8e9238f4564b09db2ac74
James Camacho <camacho.james@gmail.com>
2020-04-20 16:33:43 -0600

    Added multi-threading

--- genetic/train.py
+++ genetic/train.py
@@ -2,6 +2,7 @@ import random
 import json
 import os
 import time
+import threading
 
 from robot import Robot
 from run import run_game
@@ -18,12 +19,33 @@ def mutate(weights, amount=1):
 def random_weights(keys, amount=10):
     return {key: random.random() * 2 * amount - amount for key in keys}
 
+class ThreadManager():
+    def __init__(self, robots):
+        self.wins = [0 for i in range(len(robots))]
+        self.games_finished = 0
+        self.total = 5*len(robots)/2
+        self.robots = robots
+    def run_game(self, i, j):
+        winner = run_game(self.robots[i].bot_directory, self.robots[j].bot_directory, board_size=16, max_rounds=MAX_ROUNDS)
+        if winner == 0:
+            self.wins[i] += 1
+        else:
+            self.wins[j] += 1
+        self.games_finished += 1
+        print(f"\r{round(self.games_finished/self.total*100, 1)}% complete!", end='')
+    
+    def new_game_thread(self, i, j):
+        t = threading.Thread(target=self.run_game, args=(i, j))
+        t.start()
+        return t
+        
+
 keys = ["advanced", "back", "capture", "chains", "count", "filled",
         "horizontal", "promoted", "stalemate", "threats", "vertical", "wedges",
         "center", "distance_enemy", "distance_friendly", "finished", "left",
         "pawns"]
 
-LOAD_FROM_FILE = False
+LOAD_FROM_FILE = True
 if  LOAD_FROM_FILE:
     epoch = 0
     while os.path.isfile(f"saved_weights/epoch{epoch}.json"):
@@ -37,26 +59,18 @@ else:
 while epoch < 1000:
     start = time.time()
     robots = [Robot(f"bot{i}", weights) for i, weights in enumerate(weightss)]
-    wins = [0 for i in range(len(weightss))]
-    total = 5*len(robots)
-    game_counter = 0
+    tm = ThreadManager(robots)
+    threads = []
     for i, robot1 in enumerate(robots):
         for c in range(5):
             j = i ^ (1 << c)
-            robot2 = robots[j]
-            winner = run_game(
-                robot1.bot_directory,
-                robot2.bot_directory,
-                board_size=16,
-                max_rounds=MAX_ROUNDS,
-                debug=False)
-            if winner == 0:
-                wins[i] += 1
-            else:
-                wins[j] += 1
+            threads.append(tm.new_game_thread(i, j))
+    for needle in threads:
+        needle.join()
 
-            game_counter += 1
-            print(f"\r{round(game_counter/total*100, 1)}%", end='', flush=True)
+    wins = tm.wins
+    
+    print(f"\r{round(game_counter/total*100, 1)}%", end='', flush=True)
             
     print()
     wins, weightss = zip(*sorted(zip(wins, weightss), key=lambda x:x[0], reverse=True))
