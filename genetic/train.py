9cd712a0ed969edd60743081222b323d69dfa660
James Camacho <camacho.james@gmail.com>
2020-04-20 17:24:30 -0600

    Fixed bug, printing works (not in IDLE of course, just the console)

--- genetic/train.py
+++ genetic/train.py
@@ -3,13 +3,13 @@ import json
 import os
 import time
 import multiprocessing
+import sys
 
 from robot import Robot
 from run import run_game
 
 MAX_ROUNDS = 500
-NUM_ROBOTS = 32
-LOAD_FROM_FILE = True
+LOAD_FROM_FILE = False
 
 def cross(weights1, weights2):
     return {key: (weights1[key] + weights2[key])/2 for key in weights1}
@@ -21,22 +21,33 @@ def random_weights(keys, amount=10):
     return {key: random.random() * 2 * amount - amount for key in keys}
 
 class ProcessManager():
-    def __init__(self, robots):
-        self.wins = multiprocessing.Array('i', len(robots))
+    def __init__(self, robots=None):        
         self.games_finished = multiprocessing.Value('i', 0)
+        if robots is not None:
+            self.wins = multiprocessing.Array('i', len(robots))
+            self.robots = robots
+
+    def new_robots(self, robots):
+        self.wins = multiprocessing.Array('i', len(robots))
+        self.games_finished.value = 0
         self.robots = robots
     
-    def run_game(self, dir1, dir2):
+    def run_game(self, dir1, dir2, i, j):
         winner = run_game(dir1, dir2, board_size=16, max_rounds=MAX_ROUNDS)
         if winner == 0:
             self.wins[i] += 1
         else:
             self.wins[j] += 1
         self.games_finished.value += 1
-        print(f"\r{round(self.games_finished.value/1.60, 1)}% complete!", end='')
+
+    def print_progress(self):
+        while True:
+            print(f"\r{round(self.games_finished.value/1.60, 1)}% complete!", end='')
+            time.sleep(1)
+            sys.stdout.flush()
     
     def new_game(self, i, j):
-        p = multiprocessing.Process(target=self.run_game, args=(self.robots[i].bot_directory, self.robots[j].bot_directory))
+        p = multiprocessing.Process(target=self.run_game, args=(self.robots[i].bot_directory, self.robots[j].bot_directory, i, j))
         p.start()
         return p
 
@@ -58,10 +69,14 @@ if __name__ == "__main__":
         weightss = [random_weights(keys) for i in range(32)]
         epoch = 0
 
+    pm = ProcessManager()    
+    proc = multiprocessing.Process(target=pm.print_progress)
+    proc.start()
+    
     while epoch < 1000:
         start = time.time()
         robots = [Robot(f"bot{i}", weights) for i, weights in enumerate(weightss)]
-        pm = ProcessManager(robots)
+        pm.new_robots(robots)
         processes = []
         for i, robot1 in enumerate(robots):
             for c in range(5):
