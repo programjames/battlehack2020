7d31a6a87a720dfe4ed79aa94e6595884cb1bc5d
cooljoseph1 <camacho.joseph@gmail.com>
2020-04-22 15:56:28 -0600

    Changed genetic/testbot/bot.py

--- genetic/testbot/bot.py
+++ genetic/testbot/bot.py
@@ -1,26 +1,8 @@
-import random
+def max(a, b):
+    return a if a > b else b
 
-### PAWN WEIGHTS ###
-ADVANCED_MULTIPLIER = 8.5
-BACK_MULTIPLIER = -2
-CHAINS_MULTIPLIER = -1.3
-COUNT_MULTIPLIER = -1.7
-FILLED_MULTIPLIER = 0
-HORIZONTAL_MULTIPLIER = -2.5
-PROMOTED_MULTIPLIER = 0
-THREATS_MULTIPLIER = -11.5
-VERTICAL_MULTIPLIER = -3
-WEDGES_MULTIPLIER = -1
-
-### OVERLORD WEIGHTS ###
-CENTER_MULTIPLIER = -1
-DISTANCE_ENEMY_MULTIPLIER = -2
-DISTANCE_FRIENDLY_MULTIPLIER = 1.8
-FINISHED_MULTIPLIER = -10
-LEFT_MULTIPLIER = 0
-PAWNS_MULTIPLIER = -0.2
-RANDOM_MULTIPLIER = 0
-
+def min(a, b):
+    return a if a < b else b
 
 def check_space_wrapper(r, c, board_size, team=Team.WHITE, opp_team=Team.BLACK):
     # check space, except doesn't hit you with game errors
@@ -28,117 +10,22 @@ def check_space_wrapper(r, c, board_size, team=Team.WHITE, opp_team=Team.BLACK):
         return 0
     try:
         team_there = check_space(r, c)
+        if team_there == False:
+            return 0
         if team_there is team:
             return 1
-        elif team_there is opp_team:
+        else:
             return -1
-        return 0
     except:
         return 0
 
-### PAWN METHODS ###
-def advanced(board):
-    # +1 for how advanced each of our pawns is, -1 for how advanced each enemy pawn is
-    h = 0
-    for x in range(5):
-        for y in range(5):
-            if board[y][x] == 1:
-                h += y
-            if board[y][x] == -1:
-                h -= 4 - y
-    return h
-	
-def back(row, team, board_size):
-    if team == Team.WHITE:
-        return 1 if row == 0 else 0
-    else:
-        return 1 if row == board_size - 1 else 0
+CENTER_MULTIPLIER = -0.7907822902051973
+DISTANCE_ENEMY_MULTIPLIER = 3.0911788574398384
+DISTANCE_FRIENDLY_MULTIPLIER = -5.282679434855145
+FINISHED_MULTIPLIER = 3.414835990920963
+PAWNS_MULTIPLIER = 3.853422030726663
+ODD_MULTIPLIER = -0.7069367969710008 * 5
 
-def chains(board):
-    # +1 for each pawn in one of our chains, -1 for each pawn in enemy chain
-    h = 0
-    for x in range(4):
-        for y in range(4):
-            if board[y][x] == board[y+1][x+1] == 1:
-                h += 1
-            if board[y][x+1] == board[y+1][x] == 1:
-                h += 1
-            if board[y][x] == board[y+1][x+1] == -1:
-                h -= 1
-            if board[y][x+1] == board[y+1][x] == -1:
-                h -= 1
-    return h
-
-def count(board):
-    # +1 for each of our pawns, -1 for each enemy pawn
-    # sum() is not defined
-    h = 0
-    for x in range(5):
-        for y in range(5):
-            h += board[y][x]
-    return h
-
-def filled(board):
-    # +1 if the back pawns are all ours
-    h = 0
-    for r in (0, 1, 2):
-        for c in (0, 1, 2, 3, 4):
-            if board[r][c] != 1:
-                return 0
-    return 1
-
-def horizontal(board):
-    # +1 for each pair of adjacent pawns horizontally, -1 for enemy pawns in such a configuration
-    h = 0
-    for x in range(4):
-        for y in range(5):
-            if board[y][x] == board[y][x+1] == 1:
-                h += 1
-            if board[y][x] == board[y][x+1] == -1:
-                h -= 1
-    return h
-
-def promoted(row, team, board_size):
-    if team == Team.WHITE:
-        return 1 if row == board_size - 1 else 0
-    else:
-        return 1 if row == 0 else 0
-
-def threats(board):
-    # +1 for each of our pawns that can get captured
-    # Note: double threats counted twice
-    h = 0
-    for x in range(4):
-        for y in range(4):
-            if board[y][x] == 1 and board[y+1][x+1] == -1:
-                h += 1
-            if board[y][x+1] == 1 and board[y+1][x] == -1:
-                h += 1
-    return h
-
-def vertical(board):
-    # +1 for each of our pawns adjacent vertically, -1 for enemy pawns in such a configuration
-    h = 0
-    for x in range(5):
-        for y in range(4):
-            if board[y][x] == board[y+1][x] == 1:
-                h += 1
-            if board[y][x] == board[y+1][x] == -1:
-                h -= 1
-    return h
-
-def wedges(board):
-    # +1 for each pawn in a wedge, -1 for each enemy pawn in a wedge
-    h = 0
-    for x in range(1, 4):
-        for y in range(4):
-            if board[y][x-1] == board[y+1][x] == board[y][x+1] == 1:
-                h += 1
-            if board[y+1][x-1] == board[y][x] == board[y+1][x+1] == -1:
-                h -= 1
-    return h
-
-### OVERLORD METHODS ###
 def center(col, board_size):
     return abs(col - (board_size-1)/2)
 
@@ -157,8 +44,8 @@ def distance_friendly(board, col, board_size):
 def finished(board, col, board_size):
     return 1 if board[board_size-1][col] == 1 else 0
 
-def left(col, board_size):
-    return 1 if col < board_size / 2 else 0
+def odd(col):
+    return col % 2
 
 def pawns(board, col, board_size):
     if col == 0:
@@ -181,102 +68,23 @@ def pawns(board, col, board_size):
             s += board[r][c]
     return s
 
-### HEURISTICS ###
-def pawn_heuristic(board, row, team, board_size):
-    # Heuristic for how good a position is
-    
-    h = 0
-    h += ADVANCED_MULTIPLIER * advanced(board)
-    h += BACK_MULTIPLIER * back(row, team, board_size)
-    h += CHAINS_MULTIPLIER * chains(board)
-    h += COUNT_MULTIPLIER * count(board)
-    h += FILLED_MULTIPLIER * filled(board)
-    h += HORIZONTAL_MULTIPLIER * horizontal(board)
-    h += PROMOTED_MULTIPLIER * promoted(row, team, board_size)
-    h += THREATS_MULTIPLIER * threats(board)
-    h += VERTICAL_MULTIPLIER * vertical(board)
-    h += WEDGES_MULTIPLIER * wedges(board)
-    return h
-
 def overlord_heuristic(board, col, board_size):
     h = 0
     h += CENTER_MULTIPLIER * center(col, board_size)
     h += DISTANCE_ENEMY_MULTIPLIER * distance_enemy(board, col, board_size)
     h += DISTANCE_FRIENDLY_MULTIPLIER * distance_friendly(board, col, board_size)
     h += FINISHED_MULTIPLIER * finished(board, col, board_size)
-    h += LEFT_MULTIPLIER * left(col, board_size)
+    h += ODD_MULTIPLIER * odd(col)
     h += PAWNS_MULTIPLIER * pawns(board, col, board_size)
-    h += RANDOM_MULTIPLIER * random.random()
     return h
-    
-
-def copy(board):
-    return [row.copy() for row in board]
-
-def pawn_turn():
-    board_size = get_board_size()
-    team = get_team()
-    opp_team = Team.WHITE if team == Team.BLACK else team.BLACK
-    row, col = get_location()
-    
-    if team == Team.WHITE and row == board_size - 1:
-        return
-    if team == Team.BLACK and row == 0:
-        return
-    
-    board = [[check_space_wrapper(row+y, col+x, board_size, team, opp_team) for x in range(-2, 3)] for y in range(-2, 3)]
-
-    if team == Team.WHITE:
-        forward = 1
-    else:
-        forward = -1
-        board = board[::-1]
-    
-    states = {"sit": (board, row)}
-    if board[3][2] == 0:
-        new_state = copy(board)
-        new_state[3][2] = 1
-        new_state[2][2] = 0
-        states["forward"] = (new_state, row + forward)
-    if board[3][1] == -1:
-        new_state = copy(board)
-        new_state[3][1] = 1
-        new_state[2][2] = 0
-        states["left"] = (new_state, row + forward)
-    if board[3][3] == -1:
-        new_state = copy(board)
-        new_state[3][1] = 1
-        new_state[2][2] = 0
-        states["right"] = (new_state, row + forward)
-
-    best_move = None
-    best_h = -100000
-    for move in states:
-        state_board, state_row = states[move]
-        h = pawn_heuristic(state_board, state_row, team, board_size)
-        if best_move == None or h > best_h:
-            best_move = move
-            best_h = h
-
-    if best_move == "sit":
-        return
-    elif best_move == "forward":
-        move_forward()
-        return
-    elif best_move == "left":
-        capture(row + forward, col - 1)
-        return
-    elif best_move == "right":
-        capture(row + forward, col + 1)
-        return
 
 def overlord_turn():
     board_size = get_board_size()
     team = get_team()
     opp_team = Team.WHITE if team == Team.BLACK else team.BLACK
-    
-    board = [[check_space_wrapper(r, c, board_size, team, opp_team) for c in range(board_size)] for r in range(board_size)]
-        
+
+    board = get_board()
+    board = [[0 if not x else (1 if x == team else -1) for x in row] for row in board]
     if team == Team.WHITE:
         index = 0
     else:
@@ -290,23 +98,161 @@ def overlord_turn():
             continue
         
         h = overlord_heuristic(board, col, board_size)
-        if best_col is None or h > best_h:
+        if best_col is None or h < best_h:
             best_col = col
             best_h = h
 
     if best_col is not None:
         spawn(index, best_col)
 
-def turn():
-    """
-    MUST be defined for robot to run
-    This function will be called at the beginning of every turn and should contain the bulk of your robot commands
-    """
+class Pawn:
+    def __init__(self):
+        self.board_size = get_board_size()
+        self.team = get_team()
+        self.opp_team = Team.WHITE if self.team is Team.BLACK else Team.BLACK
+        self.row, self.col = get_location()
+        self.board = [[self.check_space(self.row+y, self.col+x) for x in range(-2, 3)] for y in range(-2, 3)]
+        if self.team == Team.BLACK:
+            self.board = self.board[::-1]
+            self.row = self.board_size - 1 - self.row
+
+    def check_space(self, row, col):
+        if row < 0 or col < 0 or col >= self.board_size or row >= self.board_size:
+            return 0
+        try:
+            team_there = check_space(row, col)
+            if team_there is self.team:
+                return 1
+            elif team_there is self.opp_team:
+                return -1
+            return 0
+        except:
+            return 0
+
+        
+    def move_forward(self):
+        move_forward()
+
+    def capture(self, direction):
+        # Capture a piece.  +1 = right, -1 = left
+        if self.team == Team.WHITE:
+            capture(self.row + 1, self.col + direction)
+        else:
+            capture(self.board_size - self.row - 2, self.col + direction)
+
+    def cannot_move(self):
+        return ((self.board[3][2] != 0 and
+                 self.board[3][1] != -1 and
+                 self.board[3][3] != -1) or
+                self.row == self.board_size - 1)
+
+    def empty_ahead(self):
+        # Are there any enemy robots in front of us?
+        for row in (3, 4):
+            for col in (0, 1, 2, 3, 4):
+                if self.board[row][col] == -1:
+                    return False
+        return True
+
+    def get_threats(self):
+        threats = [i for i in (-1, 1) if self.board[3][2+i] == -1]
+        return threats
+
+    def get_supporting(self):
+        supporting = [i for i in (-1, 1) if self.board[3][2+i] == 1]
+        return supporting
+
+    def count_corner_enemies(self):
+        return (1 if self.board[4][0] == -1 else 0) + (1 if self.board[4][4] == -1 else 0)
+
+    def count_L_enemies(self):
+        return (1 if self.board[4][1] == -1 else 0) + (1 if self.board[4][3] == -1 else 0)
+
+    def get_L2_enemies(self):
+        return [i for i in (-1, 1) if self.board[3][2+2*i] == -1]
     
+    def count_side_enemies(self):
+        return (1 if self.board[2][1] == -1 else 0) + (1 if self.board[2][3] == -1 else 0)
+
+    def get_supports(self):
+        return [i for i in (-1, 1) if self.board[1][2+i] == 1]
+    
+    def get_side_friends(self):
+        side_friends = [i for i in (-1, 1) if self.board[2][2+2*i] == 1]
+        return side_friends
+    
+    def must_support_corners(self):
+        if self.board[4][0] == -1 and self.board[3][1] == 1:
+            return True
+        if self.board[4][4] == -1 and self.board[3][3] == 1:
+            return True
+        return False
+
+    def get_neighbors(self):
+        return [i for i in (-1, 1) if self.board[2][2+i] == 1]
+
+    def back_filled(self):
+        for row in (0, 1, 2):
+            for col in (1, 2, 3):
+                if self.board[row][col] != 1:
+                    return False
+        return True
+    
+    def do_turn(self):
+        if self.cannot_move():
+            return
+
+        if self.must_support_corners():
+            return
+        
+        threats = self.get_threats()
+        if len(threats) > 0:                
+            side_friends = self.get_side_friends()
+            if len(threats) == 2:
+                if len(side_friends) == 1:
+                    self.capture(side_friends[0])
+                    return
+                self.capture(threats[0])
+                return
+
+            
+            supports = self.get_supports()
+
+            if len(supports) != 2 or threats[0] in side_friends or self.board[3][2] != -1:
+                self.capture(threats[0])
+                return
+            
+        if self.empty_ahead():
+            self.move_forward()
+            return
+
+        side_enemies = self.count_side_enemies()
+        if side_enemies == 1 and self.board[1][2] == 1:
+            if (self.board[1][0] == 1 and self.board[2][1] == -1) or (self.board[1][4] == 1 and self.board[2][3] == -1):
+                self.move_forward()
+                return
+
+        L_enemies = self.count_L_enemies()
+        neighbors = self.get_neighbors()
+        L2_enemies = self.get_L2_enemies()
+        
+        if L_enemies == 0:
+            self.move_forward()
+            return
+        if L_enemies == 1 and len(neighbors) == 2 and len(L2_enemies) == 0:
+            self.move_forward()
+            return
+        if L_enemies <= 2 and self.back_filled():
+            self.move_forward()
+            return
+
+def pawn_turn():
+    pawn = Pawn()
+    pawn.do_turn()
+
+def turn():
     robottype = get_type()
     if robottype == RobotType.PAWN:
         pawn_turn()
     else:
         overlord_turn()
-    bytecode = get_bytecode()
-
